fc6be6dd6d6c22988af8e70275f42a5c5aedd00e
fthffs <fthffs@pm.me>
2024-11-10 20:21:59 +0100

    Fixed update.sh path

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -5,7 +5,7 @@ radarr() {
   local new_version="$(curl -sSL "${radarr_url}" | jq '.[0].version' -r)"
 
   if [ "${new_version}" ]; then
-    sed -i "s/radarr_version=.*/radarr_version=${new_version}/" radarr/Dockerfile
+    sed -i "s/radarr_version=.*/radarr_version=${new_version}/" Dockerfile
   fi
 
   if output=$(git status --porcelain) && [ -z "$output" ]; then
@@ -18,61 +18,4 @@ radarr() {
   fi
 }
 
-sonarr() {
-  sonarr_url="https://services.sonarr.tv/v1/releases"
-  sonarr_channel="v4-stable"
-  local new_version=$(curl -sSL ${sonarr_url} | jq -r "first(.[] | select(.releasechannel==\"${sonarr_channel}\") | .version)")
-
-  if [ "${new_version}" ]; then
-    sed -i "s/sonarr_version=.*/sonarr_version=${new_version}/" sonarr/Dockerfile
-  fi
-
-  if output=$(git status --porcelain) && [ -z "$output" ]; then
-    # working directory clean
-    echo "no new sonarr version available!"
-  else
-    # uncommitted changes
-    git commit -a -m "updated sonarr to version: ${new_version}"
-    git push
-  fi
-}
-
-prowlarr() {
-  prowlarr_url="https://prowlarr.servarr.com/v1/update/master/changes?os=linux&runtime=netcore"
-  local new_version="$(curl -sSL "${prowlarr_url}" | jq '.[0].version' -r)"
-
-  if [ "${new_version}" ]; then
-    sed -i "s/PROWLARR_VERSION=.*/PROWLARR_VERSION=${new_version}/" prowlarr/Dockerfile
-  fi
-
-  if output=$(git status --porcelain) && [ -z "$output" ]; then
-    # working directory clean
-    echo "no new prowlarr version available!"
-  else
-    # uncommitted changes
-    git commit -a -m "updated prowlarr to version: ${new_version}"
-    git push
-  fi
-}
-
-qbittorrent() {
-  qbittorrent_url="https://api.github.com/repos/qbittorrent/qBittorrent/tags"
-  local new_version="$(curl -SsL ${qbittorrent_url} | jq -r -c '.[] | .name' | grep -iv 'rc\|beta\|alpha' | head -n 1 | sed 's/release-//')"
-
-  if [ "${new_version}" ]; then
-    sed -i "s/QBT_VERSION=.*/QBT_VERSION=${new_version}/" qbittorrent/Dockerfile
-  fi
-
-  if output=$(git status --porcelain) && [ -z "$output" ]; then
-    # working directory clean
-    echo "no new qbittorrent version available!"
-  else
-    # uncommitted changes
-    git commit -a -m "updated qbittorrent to version: ${new_version}"
-    git push
-  fi
-}
 radarr
-sonarr
-prowlarr
-qbittorrent
