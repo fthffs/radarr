23952c881eaf123287cbc1b08031279f4947bce2
fthffs <40957013+fthffs@users.noreply.github.com>
2025-01-27 19:02:10 +0100

    Update update.sh

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -2,7 +2,9 @@
 
 radarr() {
   radarr_url="https://radarr.servarr.com/v1/update/master/changes?runtime=netcore&os=linux"
+  grep 'radarr_version' Dockerfile
   local new_version="$(curl -sSL "${radarr_url}" | jq '.[0].version' -r)"
+  echo "Latest version: ${new_version}"
 
   if [ "${new_version}" ]; then
     sed -i "s/radarr_version=.*/radarr_version=${new_version}/" Dockerfile
